11f247f7eec98d4f935af3497f2aafcdbb88e71d
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-08-04 14:11:04 +0200

    Rename argument to stay in line with MDN docs

--- src/useMutationObserver.js
+++ src/useMutationObserver.js
@@ -4,12 +4,12 @@ import { useEffect } from 'react';
  * Observes a given element using MutationObserver.
  *
  * @param {HTMLElement} element Element to attach MutationObserver to
- * @param {object} config MutationObserver config. WARNING! If you define the object in
+ * @param {object} options MutationObserver options. WARNING! If you define the object in
  *   component body, make sure to memoize it.
  * @param {Function} observerCallback MutationObserver callback. WARNING! If you define
  *   the function in component body, make sure to memoize it.
  */
-export default function useMutationObserver(element, config, observerCallback) {
+export default function useMutationObserver(element, options, observerCallback) {
   useEffect(() => {
     if (!element) {
       return undefined;
@@ -17,10 +17,10 @@ export default function useMutationObserver(element, config, observerCallback) {
 
     const observer = new MutationObserver(observerCallback);
 
-    observer.observe(element, config);
+    observer.observe(element, options);
 
     return () => {
       observer.disconnect();
     };
-  }, [element, config, observerCallback]);
+  }, [element, options, observerCallback]);
 }
