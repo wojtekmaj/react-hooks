e400556130559da6100f712c96549d0ab8b744f2
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-01-09 22:25:18 +0100

    Add documentation for useWindowHeight, useWindowWidth

--- src/useWindowHeight.js
+++ src/useWindowHeight.js
@@ -1,6 +1,11 @@
 import { useCallback, useState } from 'react';
 import useEventListener from './useEventListener';
 
+/**
+ * Returns the interior height of the window in pixels.
+ *
+ * @returns {number} Height of the window in pixels
+ */
 export default function useWindowHeight() {
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
--- src/useWindowWidth.js
+++ src/useWindowWidth.js
@@ -1,6 +1,11 @@
 import { useCallback, useState } from 'react';
 import useEventListener from './useEventListener';
 
+/**
+ * Returns the interior width of the window in pixels.
+ *
+ * @returns {number} Width of the window in pixels
+ */
 export default function useWindowWidth() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
