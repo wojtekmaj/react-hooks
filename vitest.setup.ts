c63aa5846d64ac914c39015c2f4a7bc33bfacf99
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-11-17 10:11:39 +0100

    Fix TypeScript issue

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,5 +5,6 @@ global.MediaQueryListEvent = class MediaQueryListEvent extends Event {
   constructor(type: string, eventInitDict?: MediaQueryListEventInit) {
     super(type, eventInitDict);
     this.matches = eventInitDict?.matches ?? false;
+    this.media = eventInitDict?.media ?? '';
   }
 };
